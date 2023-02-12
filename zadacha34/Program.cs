2ff85d5362c35dc7b95532a5fd3c3b76ec81e804
“Ivan <ivkriv@yandex.ru>
2023-02-12 18:50:07 +0300

    resheno34

--- zadacha34/Program.cs
+++ zadacha34/Program.cs
@@ -2,7 +2,7 @@
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
+Console.WriteLine("Вот массив по вашему запросу: ");
 PrintArray(numbers);
 int count = 0;
 
@@ -10,7 +10,7 @@ for (int z = 0; z < numbers.Length; z++)
 if (numbers[z] % 2 == 0)
 count++;
 
-Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+Console.WriteLine($"всего в массиве {numbers.Length} чисел, {count} из них чётные");
 
 void FillArrayRandomNumbers(int[] numbers)
 {
